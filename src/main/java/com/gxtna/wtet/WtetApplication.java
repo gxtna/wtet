a6e14a486688b7569fe84ed66feb9eb2d4329e39
gxtna <15830407570@163.com>
2022-11-19 20:32:49 +0800

    frist commit

---
+++ src/main/java/com/gxtna/wtet/WtetApplication.java
@@ -0,0 +1,13 @@
+package com.gxtna.wtet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gxtna/wtet/WtetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxtna.wtet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
